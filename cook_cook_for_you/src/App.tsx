12787d50d9c20586a216545c85cce46e4588d0a4
Crystal Chen <crystal.r.chen@gmail.com>
2023-11-19 21:02:15 +0800

    setting enviroment

---
+++ cook_cook_for_you/src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from "react";
+import "./App.css";
+//////
+function App() {
+  const name: string = "Crystal";
+  const hellworld: string = "hello";
+
+  return (
+    <>
+      <h1>{name}</h1>
+      <h2>{hellworld}</h2>
+    </>
+  );
+}
+
+export default App;
