7d2b14ba43afba9ffd498f4bb259ceb0cddddac5
Crystal Chen <crystal.r.chen@gmail.com>
2023-11-23 15:05:50 +0800

    refactor:  change the firebaseConfig placement
    Move firebaseConfig to a more accessible location for better readability and recognition.
    Export Firebase services for easy access:

--- cook_cook_for_you/src/main.tsx
+++ cook_cook_for_you/src/main.tsx
@@ -1,11 +1,16 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App.tsx";
-
+import Profile from "./Profile/index.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  </BrowserRouter>
+  // </React.StrictMode>
 );
