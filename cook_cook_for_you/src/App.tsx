b58ef116eb7cb28c029f43a8d2066711d91c2346
Crystal Chen <crystal.r.chen@gmail.com>
2023-12-06 16:31:09 +0800

    refactor: Refine global style settings ＆Adjust route configurations
    - Adjust global style settings for a refined and consistent visual appearance.
    - Fine-tune route settings to optimize navigation and user experience.

--- cook_cook_for_you/src/App.tsx
+++ cook_cook_for_you/src/App.tsx
@@ -1,29 +1,49 @@
-import { StepForwardOutlined } from "@ant-design/icons";
-import { Button, Space } from "antd";
-import dayjs from "dayjs";
 import "dayjs/locale/zh-cn";
 import "firebase/database";
 import React from "react";
-import styled from "styled-components";
-import AddDailyMeal from "./AddDailyMeal";
-dayjs.locale("zh-cn");
+import { Outlet } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+import Header from "./components/Header";
+import { auth } from "./firbase";
 
-const Wrapper = styled.div`
-  margin: 40px;
-  padding: 20px;
-  border-radius: 20px;
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Noto Sans TC', sans-serif;
+  }
+
+  #root {
+    min-height: 100vh;
+    padding: 140px 0 115px;
+    position: relative;
+
+    @media screen and (max-width: 1279px) {
+      padding: 102px 0 208px;
+    }
+  }
 `;
+
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    // 使用者已登入
+    console.log("使用者已登入:", user);
+  } else {
+    // 使用者已登出
+    console.log("使用者已登出");
+  }
+});
 
 const App: React.FC = () => {
   return (
-    <Wrapper>
-      <Space>
-        <Button type="primary">
-          <StepForwardOutlined />
-        </Button>
-      </Space>
-      <AddDailyMeal />
-    </Wrapper>
+    <>
+      <GlobalStyle />
+      <Header />
+      <Outlet />
+      {/* <AddDailyMeal /> */}
+    </>
   );
 };
 
--- cook_cook_for_you/src/main.tsx
+++ cook_cook_for_you/src/main.tsx
@@ -2,7 +2,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App.tsx";
 import Profile from "./Profile/index.tsx";
-import "./index.css";
+// import "./index.css";
+import AddDailyMeal from "./AddDailyMeal/index.tsx";
 import RecipeList from "./pages/Recipes/RecipeForm.tsx";
 import Recipes from "./pages/Recipes/index.tsx";
 
@@ -10,11 +11,13 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/recipes" element={<Recipes />} />
-      <Route path="/recipelist" element={<RecipeList />} />
-      RecipeList
+      <Route path="/" element={<App />}>
+        <Route path="/dailymealplan" element={<AddDailyMeal />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/recipes" element={<Recipes />} />
+        <Route path="/recipelist" element={<RecipeList />} />
+        RecipeList
+      </Route>
     </Routes>
   </BrowserRouter>
   // </React.StrictMode>
