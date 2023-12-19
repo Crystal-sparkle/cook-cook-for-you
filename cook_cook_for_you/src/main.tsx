8473334d077b2fb9408bc957421a0d156bcd457d
Crystal Chen <crystal.r.chen@gmail.com>
2023-12-19 17:16:05 +0800

    feat: Configure routing in the application

--- cook_cook_for_you/src/main.tsx
+++ cook_cook_for_you/src/main.tsx
@@ -1,23 +1,13 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App.tsx";
-import Profile from "./Profile/index.tsx";
 // import "./index.css";
-import AddDailyMeal from "./AddDailyMeal/index.tsx";
-import RecipeList from "./pages/Recipes/RecipeForm.tsx";
-import Recipes from "./pages/Recipes/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="/dailymealplan" element={<AddDailyMeal />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/recipes" element={<Recipes />} />
-        <Route path="/recipelist" element={<RecipeList />} />
-        RecipeList
-      </Route>
+      <Route path="/*" element={<App />}></Route>
     </Routes>
   </BrowserRouter>
   // </React.StrictMode>
