cb794bb49092d321384749c38b5496f7b52cf847
Crystal Chen <crystal.r.chen@gmail.com>
2024-03-28 16:09:44 +0800

    modify  type  and breakpoints

--- src/utils/breakpoints.tsx
+++ src/utils/breakpoints.tsx
@@ -1,9 +1,11 @@
 const size = {
   mobile: "1279px",
+  pad: "768px",
   desktop: "1280px",
 };
 
 export const device = {
   mobile: `(max-width: ${size.mobile})`,
+  pad: `(max-width: ${size.pad})`,
   desktop: `(min-width: ${size.desktop})`,
 };
